4f70d84a90b151875b2514b39a001c6e7419485b
bthnpolt <139389577+bthnpolt@users.noreply.github.com>
2025-01-04 03:06:03 +0300

    responsive navbar

---
+++ js/bar.js
@@ -0,0 +1,13 @@
+const menuBar = document.getElementById("menu_bar");
+const navLinks = document.querySelector(".nav_links");
+
+ navLinks.style.top = '-450px';
+
+menuBar.onclick = function (){
+    if (navLinks.style.top == '-450px') {
+        navLinks.style.top = '50px';
+    }
+    else{
+        navLinks.style.top = '-450px';
+    }
+}
